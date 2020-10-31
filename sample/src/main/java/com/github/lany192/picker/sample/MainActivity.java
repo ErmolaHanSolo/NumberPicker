f58919068ee98ee8d66699e468274a281e70f325
lany <lany192@qq.com>
2020-10-31 19:28:01 +0800

    update build version

--- sample/src/main/java/com/github/lany192/picker/sample/MainActivity.java
+++ sample/src/main/java/com/github/lany192/picker/sample/MainActivity.java
@@ -1,7 +1,8 @@
 package com.github.lany192.picker.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.lany192.picker.NumberPicker;
 
